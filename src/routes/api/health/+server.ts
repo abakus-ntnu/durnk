09b62fea42f4958510c2716b4780e30d42de854d
Henrik Tøn Løvhaug <henrik.lovhaug@gmail.com>
2025-03-11 12:04:42 +0100

    Improve health check

--- src/routes/api/health/+server.ts
+++ src/routes/api/health/+server.ts
@@ -1,5 +1,11 @@
 import type { RequestHandler } from '@sveltejs/kit';
+import { client } from '../../../mongo';
 
-export const GET: RequestHandler = ({}) => {
+export const GET: RequestHandler = async ({}) => {
+  // Ping DB
+  const restult = await client.db('bull-db').command({ ping: 1 });
+  if (!restult.ok) {
+    return new Response('MongoDB failed to respond', { status: 500 });
+  }
 	return new Response('OK', { status: 200 });
 };
